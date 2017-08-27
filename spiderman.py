d1330abcc87cbb6c0548482718071ff4cf4cd98d
stsupertam <supmantam@gmail.com>
2017-08-27 23:51:12 +0700

    add LinkParser template

---
+++ spiderman.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+class LinkParser():
+
+    def get_link(self):
+        pass
+
+    def check_duplicate(self):
+        pass
+
+
+def spider():
+    pass
+    

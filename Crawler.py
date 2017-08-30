b4c9db5c500c15e72303265da75f13bee76f5612
stsupertam <supmantam@gmail.com>
2017-08-31 01:25:15 +0700

    seperate check content-type and status code to different if/else

--- Crawler.py
+++ Crawler.py
@@ -16,7 +16,7 @@ class Spider():
         self.total_pages = total_pages
         self.pages = 0
         self.total_file = 0
-        self.acccpet_file = set(['text/html', 'application/xml', 'application/xhtml+xml'])
+        self.accept_file = set(['text/html', 'application/xml', 'application/xhtml+xml'])
 
     def startCrawl(self):
         u_parse = urlparse(self.url)
@@ -46,25 +46,33 @@ class Spider():
         if(tldextract.extract(self.domain).domain == tldextract.extract(domain).domain):
             try:
                 print('Retrieving [%s] %s' % (domain, url))
-                data  = requests.get(('%s://%s%s' % (self.scheme, domain, url)), timeout=5)
-                content_type = data.headers['content-type'].split(';')[0]
-                status_code = data.status_code
-                soup = BeautifulSoup(data.text, 'lxml')
+                req_header  = requests.head(('%s://%s%s' % (self.scheme, domain, url)), timeout=5)
+                content_type = req_header.headers['content-type'].split(';')[0]
 
-                if(status_code != requests.codes.ok or content_type not in self.acccpet_file):
-                    print('Retrieving Failed')
+                if(content_type not in self.accept_file):
                     self.pages -= 1
+                    print('Retrieving Failed [Content-type isn\'t in Accept file]')
                     return links
 
-                self.writeToFile(domain, soup)
+                else:
+                    data = requests.get(('%s://%s%s' % (self.scheme, domain, url)), timeout=5)
+                    status_code = data.status_code
 
-                for tag in soup.findAll('a', href=True):
-                    absolute_url = urljoin(self.domain, tag['href'])
-                    if(absolute_url not in self.url_sets):
-                        if(self.pages <= self.total_pages):
-                            self.url_sets.add(absolute_url)
-                            links.append(absolute_url)
-                            self.pages += 1
+                    if(status_code != requests.codes.ok):
+                        self.pages -= 1
+                        print('Retrieving Failed [Code : %d]' % status_code)
+                        return links
+
+                    soup = BeautifulSoup(data.text, 'lxml')
+                    self.writeToFile(domain, soup)
+
+                    for tag in soup.findAll('a', href=True):
+                        absolute_url = urljoin(self.domain, tag['href'])
+                        if(absolute_url not in self.url_sets):
+                            if(self.pages <= self.total_pages):
+                                self.url_sets.add(absolute_url)
+                                links.append(absolute_url)
+                                self.pages += 1
 
             except Exception:
                 self.pages -= 1
@@ -88,7 +96,6 @@ class Spider():
 
 start_time = time.time()
 site = 'http://www.ku.ac.th/web2012/index.php?c=adms&m=mainpage1'
-#site = 'https://stackoverflow.com'
 spider = Spider(site, 10, 10000)
 spider.startCrawl()
 print('Crawl [%s] Successful' % urlparse(site).netloc) 

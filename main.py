1b744ea5b8c9b065910a1d96a80abe3bd13da315
stsupertam <supmantam@gmail.com>
2017-09-04 11:38:47 +0700

    fix main missing urlparse

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import sys
 import getopt
 from Crawler import Spider
-
+from urllib.parse import urlparse
 
 def main():
-    init(autoreset=True)
     site = 'http://www.ku.ac.th/web2012/index.php?c=adms&m=mainpage1'
     spider = Spider(site, 100, 30000)
     spider.startCrawl()
-    print(Fore.GREEN + 'Crawl [%s] Successful' % urlparse(site).netloc)
+    print('Crawl [%s] Successful' % urlparse(site).netloc)
 
 if __name__ == "__main__":
     main()
